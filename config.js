4a5007c56dd9b1ee47822953e83a8b9aa5548b4d
galoloterias5-debug <galoloterias5@gmail.com>
2026-01-30 05:32:18 -0300

    Change opening time from 10:00 to 5:00

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.CONFIG = window.CONFIG || {};
 window.CONFIG.HOURS = {
   // Horário de funcionamento (24h)
   // Aberto todos os dias de 10:00 até 01:00 (cruza meia-noite)
-  openStart: "10:00",
+  openStart: "5:00",
   openEnd: "06:00",
   days: "Segunda a segunda"
 };
