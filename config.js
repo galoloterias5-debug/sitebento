b4b599f03a517d0d05a9359d8a29c754b265081d
galoloterias5-debug <galoloterias5@gmail.com>
2026-01-30 05:39:10 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// Configurações editáveis do site
+window.CONFIG = window.CONFIG || {};
+window.CONFIG.HOURS = {
+  // Horário de funcionamento (24h)
+  // Aberto todos os dias de 10:00 até 01:00 (cruza meia-noite)
+  openStart: "10:00",
+  openEnd: "01:00",
+  days: "Segunda a segunda"
+};
