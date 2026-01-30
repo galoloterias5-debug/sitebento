994433032fc9ac9041c16fc7aaec64a7833b4a50
galoloterias5-debug <galoloterias5@gmail.com>
2026-01-30 05:38:47 -0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// Configurações editáveis do site
-window.CONFIG = window.CONFIG || {};
-window.CONFIG.HOURS = {
-  // Horário de funcionamento (24h)
-  // Aberto todos os dias de 10:00 até 01:00 (cruza meia-noite)
-  openStart: "10:00",
-  openEnd: "08:00",
-  days: "Segunda a segunda"
-};
