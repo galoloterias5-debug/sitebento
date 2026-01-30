5070e00982d8134db27496799ed926a4b3c7f7e4
galoloterias5-debug <galoloterias5@gmail.com>
2026-01-30 05:31:05 -0300

    Update closing time to 06:00 in config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ window.CONFIG.HOURS = {
   // Horário de funcionamento (24h)
   // Aberto todos os dias de 10:00 até 01:00 (cruza meia-noite)
   openStart: "10:00",
-  openEnd: "01:00",
+  openEnd: "06:00",
   days: "Segunda a segunda"
 };
